d789422d1ab9ddc8aa7c0752e2f5e3865482baa3
彪彪 <biaobiao9966@gmail.com>
2025-08-11 13:04:56 +0800

    new

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,14 +2,14 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id);
+
   return {
     props: {
       postData,
     },
   };
 }
-
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
@@ -26,6 +26,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
